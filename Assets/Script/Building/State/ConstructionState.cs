2ebab74ea42a0a74172d0910850e963d633df97f
Arsa <umarsetyawan10@gmail.com>
2022-02-10 13:54:09 +0700

    Construction State WIP

--- Assets/Script/Building/State/ConstructionState.cs
+++ Assets/Script/Building/State/ConstructionState.cs
@@ -6,6 +6,9 @@ public class ConstructionState : BaseState
 {
 
     private GameObject _Building;
+    private int _Progress;
+    private float _BuiltTime = 0.5f;
+    private float _CurrentTime;
     public override void StartState(GameObject Unit)
     {
         base.StartState(Unit);
@@ -16,7 +19,20 @@ public class ConstructionState : BaseState
     public override void UpdateState(float DeltaTime)
     {
         base.UpdateState(DeltaTime);
-        Debug.Log("Constructing");
+        Debug.Log(_Progress);
+        _CurrentTime += DeltaTime;
+        if (_Progress < 100)
+        {
+            if (_CurrentTime >= _BuiltTime)
+            {
+                _Progress++;
+                _CurrentTime = 0;
+            }
+        }
+        else
+        {
+            Debug.Log("Finnished Construction");
+        }
     }
 
     public override void TerminateState()
